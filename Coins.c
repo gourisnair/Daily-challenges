fd808f36b37eb17dbddc935e59d17435d2541e6c
gourisnair <gouri.gopan@gmail.com>
2019-04-22 16:27:19 +0530

    A c program to find the coin exchange

---
+++ Coins.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define size 10
+#define max 20
+int a[size] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
+
+int coinExchange(int amt) {
+  int list[max], j = 0;
+  for(int i = size-1; i >= 0; i--) {
+    while(amt >= a[i]) {
+      amt = amt - a[i];
+      list[j++] = a[i];
+    }
+  }
+
+  for(int i = 0; i < j; i++) {
+    printf("%d\t", list[i]);
+  }
+}
+
+int main()
+{
+  int amt;
+  printf("Enter the amount for change: ");
+  scanf("%d", &amt);
+  coinExchange(amt);
+}
