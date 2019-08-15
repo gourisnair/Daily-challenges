349bb9ff48f7119de5b076df11297cc4c90a79cd
gourisnair <gouri.gopan@gmail.com>
2019-08-15 13:29:45 +0530

    To find the number of pairs of socks without repetition

--- HR_SockMerchant.cpp
+++ HR_SockMerchant.cpp
@@ -1,27 +1,24 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int main() {
   int n, count = 0;
-  int a[n];
-  cin>>n;
-  cout<<n;
+  cin >> n;
+  vector<int> v(n);
   for(int i = 0; i < n; i++) {
-    cin>>a[i];
+    cin >> v[i];
   }
-
-  for(int i = 0; i < n; i++) {
-    cout<<a[i];
-  }
-
-/*  for(int i = 0; i < n-1; i++) {
-    for(int j = i+1; j < n; j++) {
-      if(a[i] != 0 && a[j] != 0 && (a[i] == a[j]) && (i!=j)) {
-        count++;
-        a[i] == 0;
-        a[j] == 0;
+  for(int i = 0; i < n-1; i++) {
+    if(v[i] != 0) {
+      for(int j = i+1; j < n; j++) {
+        if(v[i] == v[j]) {
+          count++;
+          v[i] = 0;
+          v[j] = 0;
+          break;
+        }
       }
     }
-  }*/
-
-  cout << count;
+  }
+    cout << count;
 }
